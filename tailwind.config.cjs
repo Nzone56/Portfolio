073f804c08b52eedf39ae23100896504a38ab34c
nzone56 <jpereirap@unal.edu.co>
2023-07-05 21:59:04 -0500

    Start aniomation almost done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+   theme: {
+      extend: {
+         animation: {
+            fade: 'fade 1.5s linear, init 8s linear ',
+            typingtic:
+               'typing 2.5s steps(40, end), blink-caret .75s step-end infinite',
+            //
+         },
+      },
+   },
+   plugins: [],
 }
