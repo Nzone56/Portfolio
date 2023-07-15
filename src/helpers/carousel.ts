f637c260da07b9cde216a4973ce4e309a3aaa5d6
nzone56 <jpereirap@unal.edu.co>
2023-07-15 15:01:41 -0500

    Carousel fix

--- src/helpers/carousel.ts
+++ src/helpers/carousel.ts
@@ -1,46 +1,71 @@
-document.addEventListener('DOMContentLoaSded', function () {
-   const carouselImages = document.querySelector(
-      '.carousel-images'
-   ) as HTMLElement
-   const carouselImageWidth = carouselImages.clientWidth
-   console.log(carouselImageWidth)
-   let currentPosition = 0
-   let intervalId
+const buttonPrev = document.getElementById('prev')
+const buttonNext = document.getElementById('next')
+let carouselImages, carouselImageWidth, currentPosition, intervalId
 
-   const buttonPrev = document.getElementById('prev')
-   const buttonNext = document.getElementById('next')
+const observeSectionDisplay = () => {
+   const pageSection = document.getElementById('page')
+   const observer = new MutationObserver((mutationsList) => {
+      for (const mutation of mutationsList) {
+         if (
+            mutation.type === 'attributes' &&
+            mutation.attributeName === 'style'
+         ) {
+            const { display } = getComputedStyle(pageSection)
+            if (display === 'block') {
+               startCarousel()
+            } else {
+               stopCarousel()
+            }
+         }
+      }
+   })
+   observer.observe(pageSection, { attributes: true })
+}
 
-   const slideNext = () => {
-      console.log(currentPosition, carouselImageWidth)
-      currentPosition -= carouselImageWidth
+const slideNext = () => {
+   console.log(currentPosition, carouselImageWidth)
+   currentPosition -= carouselImageWidth
 
-      if (
-         currentPosition <
+   if (
+      currentPosition <
+      -carouselImageWidth * (carouselImages.children.length - 1)
+   ) {
+      currentPosition = 0
+   }
+   carouselImages.style.transform = `translateX(${currentPosition}px)`
+
+   clearInterval(intervalId)
+   intervalId = setInterval(slideNext, 3000)
+}
+
+const slidePrev = () => {
+   console.log('work')
+   currentPosition += carouselImageWidth
+   if (currentPosition > 0) {
+      currentPosition =
          -carouselImageWidth * (carouselImages.children.length - 1)
-      ) {
-         currentPosition = 0
-      }
-      carouselImages.style.transform = `translateX(${currentPosition}px)`
-
-      clearInterval(intervalId)
-      intervalId = setInterval(slideNext, 3000)
    }
+   carouselImages.style.transform = `translateX(${currentPosition}px)`
 
-   const slidePrev = () => {
-      console.log('work')
-      currentPosition += carouselImageWidth
-      if (currentPosition > 0) {
-         currentPosition =
-            -carouselImageWidth * (carouselImages.children.length - 1)
-      }
-      carouselImages.style.transform = `translateX(${currentPosition}px)`
+   clearInterval(intervalId)
+   intervalId = setInterval(slideNext, 3000)
+}
 
-      clearInterval(intervalId)
-      intervalId = setInterval(slideNext, 3000)
-   }
+const startCarousel = () => {
+   carouselImages = document.querySelector('.carousel-images')
+   carouselImageWidth = carouselImages.clientWidth
+   currentPosition = 0
 
    intervalId = setInterval(slideNext, 3000)
 
    buttonPrev.addEventListener('click', slidePrev)
    buttonNext.addEventListener('click', slideNext)
-})
+}
+
+const stopCarousel = () => {
+   clearInterval(intervalId)
+   buttonPrev.removeEventListener('click', slidePrev)
+   buttonNext.removeEventListener('click', slideNext)
+}
+
+observeSectionDisplay()
